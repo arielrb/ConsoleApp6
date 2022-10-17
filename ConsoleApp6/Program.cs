98df0f12ea0918cf76d8830695da1c2feeecd19d
arielrb <restagno31@gmail.com>
2022-10-17 16:30:56 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        /// <summary>
+        /// Este es un metodo main que me permite saludar al mundo
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //string dasdsdasd;
+        }
+    }
+}
